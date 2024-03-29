dbcb410c9581024d799427051b3404c22f5e6380
Abhishek Singh <aahabhisheksingh@gmail.com>
2024-03-29 12:43:39 +0530

    env setup

---
+++ components/loading.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import React from 'react'
+
+export default function Loading() {
+  return (
+    <div className='h-full w-full flex flex-col gap-y-4 justify-center items-center'>
+        <Image src="/logo.svg" alt="logo" width={120} height={120} className='animate-pulse duration-700' />
+    </div>
+  )
+}
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default{
+    providers:[
+        {
+            domain:"https://brave-owl-67.clerk.accounts.dev",
+            applicationID:"convex",
+        }
+    ]
+};
