0d0e186e88e359542943a51b4d749b5dbbd42334
Abhishek Singh <aahabhisheksingh@gmail.com>
2024-04-04 19:31:35 +0530

    sdf

--- app/api/liveblocks-auth/route.ts
+++ app/api/liveblocks-auth/route.ts
@@ -9,17 +9,13 @@ const convex = new ConvexHttpClient(
 );
 
 const liveblocks = new Liveblocks({
-secret:"sk_dev_qBBQI1XW7p6oipzIqMWpvmjXh14cKTfm6k54Ni7W4vi9sxWbGK4RZrLL1n8oyVJR"
+secret: process.env.LIVEBLOCKS_SECRET_KEY!
 });
 
 export async function POST(request: Request) {
   const authorization = await auth();
   const user = await currentUser();
 
-  console.log("AUTH_INFO", {
-    authorization,
-    user,
-  })
 
   if (!authorization || !user) {
     return new Response("Unauthorized", { status: 403 });
@@ -27,12 +23,7 @@ export async function POST(request: Request) {
 
   const { room } = await request.json();
   const board = await convex.query(api.board.get, { id: room });
-  console.log("AUTH_INFO", {
-    room,
-    board,
-    boardOrgId: board?.orgId,
-    userOrgId:authorization.orgId,
-  })
+ 
 
   if (board?.orgId !== authorization.orgId) {
     return new Response("Unauthorized", { status: 403 });
@@ -43,7 +34,6 @@ export async function POST(request: Request) {
     picture: user.imageUrl,
   };
 
-  console.log({userInfo})
 
   const session = liveblocks.prepareSession(
     user.id,
@@ -55,6 +45,5 @@ export async function POST(request: Request) {
   }
 
   const { status, body } = await session.authorize();
-  console.log({status,body},"ALLOWED");
   return new Response(body, { status });
 };
